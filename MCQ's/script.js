fe5c96b5674ee30ff206839399a77e35a40215a6
Arav <106917600+Arav9b@users.noreply.github.com>
2022-07-04 15:25:10 +0530

    Add files via upload

---
+++ MCQ's/script.js
@@ -0,0 +1,235 @@
+// Menu \\
+let menuBtn = document.getElementById("menuBtn");
+let navLinks = document.getElementById("navLinks");
+let menu = document.getElementById("menu");
+navLinks.style.right = "-250px";
+menuBtn.onclick = function(){
+    if (navLinks.style.right == "-250px") {
+        navLinks.style.right = "0";
+        menu.src = "images/close.png";
+    }
+    else {
+        navLinks.style.right = "-250px";
+        menu.src = "images/menu.png";
+    }
+}
+// MCQ's Question 1
+let btn1 = document.getElementById("btn1");
+function showAnsA(){
+    document.getElementById("answer1").style.display = "block";
+    console.log("Answer 1 is visible");
+}
+let btn2 = document.getElementById("btn2");
+function hideAnsA(){
+    document.getElementById("answer1").style.display = "none";
+    console.log("Answer 1 is hidden");
+}
+// Question 2
+let btn3 = document.getElementById("btn3");
+function showAnsB(){
+    document.getElementById("answer2").style.display = "block";
+    console.log("Answer 2 is visible");
+}
+let btn4 = document.getElementById("btn4");
+function hideAnsB(){
+    document.getElementById("answer2").style.display = "none";
+    console.log("Answer 2 is hidden");
+}
+// Question 3
+let btn5 = document.getElementById("btn5");
+function showAnsC(){
+    document.getElementById("answer3").style.display = "block";
+    console.log("Answer 3 is visible");
+}
+let btn6 = document.getElementById("btn6");
+function hideAnsC(){
+    document.getElementById("answer3").style.display = "none";
+    console.log("Answer 3 is hidden");
+}
+// Question 4
+let btn7 = document.getElementById("btn7");
+function showAnsD(){
+    document.getElementById("answer4").style.display = "block";
+    console.log("Answer 4 is visible");
+}
+let btn8 = document.getElementById("btn8");
+function hideAnsD(){
+    document.getElementById("answer4").style.display = "none";
+    console.log("Answer 4 is hidden");
+}
+// Question 5
+let btn9 = document.getElementById("btn9");
+function showAnsE(){
+    document.getElementById("answer5").style.display = "block";
+    console.log("Answer 5 is visible");
+}
+let btn10 = document.getElementById("btn10");
+function hideAnsE(){
+    document.getElementById("answer5").style.display = "none";
+    console.log("Answer 5 is hidden");
+}
+// Question 6
+let btn11 = document.getElementById("btn11");
+function showAnsF(){
+    document.getElementById("answer6").style.display = "block";
+    console.log("Answer 6 is visible");
+}
+let btn12 = document.getElementById("btn12");
+function hideAnsF(){
+    document.getElementById("answer6").style.display = "none";
+    console.log("Answer 6 is hidden");
+}
+// Question 7
+let btn13 = document.getElementById("btn13");
+function showAnsG(){
+    document.getElementById("answer7").style.display = "block";
+    console.log("Answer 7 is visible");
+}
+let btn14 = document.getElementById("btn14");
+function hideAnsG(){
+    document.getElementById("answer7").style.display = "none";
+    console.log("Answer 7 is hidden");
+}
+// Question 8
+let btn15 = document.getElementById("btn15");
+function showAnsH(){
+    document.getElementById("answer8").style.display = "block";
+    console.log("Answer 8 is visible");
+}
+let btn16 = document.getElementById("btn16");
+function hideAnsH(){
+    document.getElementById("answer8").style.display = "none";
+    console.log("Answer 8 is hidden");
+}
+// Question 9
+let btn17 = document.getElementById("btn17");
+function showAnsI(){
+    document.getElementById("answer9").style.display = "block";
+    console.log("Answer 9 is visible");
+}
+let btn18 = document.getElementById("btn18");
+function hideAnsI(){
+    document.getElementById("answer9").style.display = "none";
+    console.log("Answer 9 is hidden");
+}
+// Question 10
+let btn19 = document.getElementById("btn19");
+function showAnsJ(){
+    document.getElementById("answer10").style.display = "block";
+    console.log("Answer 10 is visible");
+}
+let btn20 = document.getElementById("btn20");
+function hideAnsJ(){
+    document.getElementById("answer10").style.display = "none";
+    console.log("Answer 10 is hidden");
+}
+// Question 11
+let btn21 = document.getElementById("btn21");
+function showAnsK(){
+    document.getElementById("answer11").style.display = "block";
+    console.log("Answer 11 is visible");
+}
+let btn22 = document.getElementById("btn22");
+function hideAnsK(){
+    document.getElementById("answer11").style.display = "none";
+    console.log("Answer 11 is hidden");
+}
+// Question 12
+let btn23 = document.getElementById("btn23");
+function showAnsL(){
+    document.getElementById("answer12").style.display = "block";
+    console.log("Answer 12 is visible");
+}
+let btn24 = document.getElementById("btn24");
+function hideAnsL(){
+    document.getElementById("answer12").style.display = "none";
+    console.log("Answer 12 is hidden");
+}
+// Question 13
+let btn25 = document.getElementById("btn25");
+function showAnsM(){
+    document.getElementById("answer13").style.display = "block";
+    console.log("Answer 13 is visible");
+}
+let btn26 = document.getElementById("btn26");
+function hideAnsM(){
+    document.getElementById("answer13").style.display = "none";
+    console.log("Answer 13 is hidden");
+}
+// Question 14
+let btn27 = document.getElementById("btn27");
+function showAnsN(){
+    document.getElementById("answer14").style.display = "block";
+    console.log("Answer 14 is visible");
+}
+let btn28 = document.getElementById("btn28");
+function hideAnsN(){
+    document.getElementById("answer14").style.display = "none";
+    console.log("Answer 14 is hidden");
+}
+// Question 15
+let btn29 = document.getElementById("btn29");
+function showAnsO(){
+    document.getElementById("answer15").style.display = "block";
+    console.log("Answer 15 is visible");
+}
+let btn30 = document.getElementById("btn30");
+function hideAnsO(){
+    document.getElementById("answer15").style.display = "none";
+    console.log("Answer 15 is hidden");
+}
+// Question 16
+let btn31 = document.getElementById("btn31");
+function showAnsP(){
+    document.getElementById("answer16").style.display = "block";
+    console.log("Answer 16 is visible");
+}
+let btn32 = document.getElementById("btn32");
+function hideAnsP(){
+    document.getElementById("answer16").style.display = "none";
+    console.log("Answer 16 is hidden");
+}
+// Question 17
+let btn33 = document.getElementById("btn33");
+function showAnsQ(){
+    document.getElementById("answer17").style.display = "block";
+    console.log("Answer 17 is visible");
+}
+let btn34 = document.getElementById("btn34");
+function hideAnsQ(){
+    document.getElementById("answer17").style.display = "none";
+    console.log("Answer 17 is hidden");
+}
+// Question 18
+let btn35 = document.getElementById("btn35");
+function showAnsR(){
+    document.getElementById("answer18").style.display = "block";
+    console.log("Answer 18 is visible");
+}
+let btn36 = document.getElementById("btn36");
+function hideAnsR(){
+    document.getElementById("answer18").style.display = "none";
+    console.log("Answer 18 is hidden");
+}
+// Question 19
+let btn37 = document.getElementById("btn37");
+function showAnsS(){
+    document.getElementById("answer19").style.display = "block";
+    console.log("Answer 19 is visible");
+}
+let btn38 = document.getElementById("btn38");
+function hideAnsS(){
+    document.getElementById("answer19").style.display = "none";
+    console.log("Answer 19 is hidden");
+}
+// Question 20
+let btn39 = document.getElementById("btn39");
+function showAnsT(){
+    document.getElementById("answer20").style.display = "block";
+    console.log("Answer 20 is visible");
+}
+let btn40 = document.getElementById("btn40");
+function hideAnsT(){
+    document.getElementById("answer20").style.display = "none";
+    console.log("Answer 20 is hidden");
+}
